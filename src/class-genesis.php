719795ad7b7f95ee3f2c96ec7a0d9e0ddff0f447
Zachary Watkins <watkinza@gmail.com>
2019-08-28 11:32:26 -0500

    Change header background color to dark blue and overlapping page elements to lighter colors

--- src/class-genesis.php
+++ src/class-genesis.php
@@ -429,7 +429,7 @@ class Genesis {
 	 */
 	public function add_logo( $title, $inside, $wrap ) {
 
-		$logo = sprintf( '<img src="%s">', AGTODAY_THEME_DIRURL . '/images/logo.svg' );
+		$logo = sprintf( '<img src="%s">', AGTODAY_THEME_DIRURL . '/images/logo-light.svg' );
 		$home = trailingslashit( home_url() );
 
 		$new_inside = sprintf(
